3b7a51152b36a23393a6441e7350d803b2c6b70b
Antonio Cruz <logus2k@gmail.com>
2026-02-22 21:45:47 +0000

    First part of consolidating training with reporting notebooks into a single notebook

---
+++ report/genetic/evolution/genome.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+from evolution.utils import compute_genome_size
+
+
+def create_genome(input_size, hidden1_size, hidden2_size, output_size):
+    genome_size = compute_genome_size(input_size, hidden1_size, hidden2_size, output_size)
+    genome = np.random.uniform(-1, 1, genome_size)
+    return genome
